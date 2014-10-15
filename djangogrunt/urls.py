cb895ae062b2c596bb2961210616579485db07da
Guilherme Louro <guilherme-louro@hotmail.com>
2014-10-15 01:02:42 -0300

    Added Grunt compass in Gruntfile

---
+++ djangogrunt/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from core.views import HomeView
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', HomeView.as_view(), name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
